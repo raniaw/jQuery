aa2b2523acb64d139818e3b5ddec02ad68a47f89
raniaw <raniaw@web.de>
2019-09-04 11:20:23 +0200

    new file

---
+++ lesson11/js/script.js
@@ -0,0 +1,52 @@
+var template = $("#hb-each").html();
+//compile the template
+var templateScript = Handlebars.compile(template);
+
+var content = {
+    "mylist": ["Mascha", "Medved"]
+}
+
+var html = templateScript(content);
+
+//insert html code in the page
+$(document.body).append(html);
+
+
+
+
+
+/**
+ * Funktion für die zufällige dunkle Farbeerstellung
+ */
+
+
+function colorCreateDark() {
+    var color;
+    var max = 160;
+    var min = 0;
+
+    var colRed = Math.floor(Math.random() * (max - min)) + min;
+    var colBlue = Math.floor(Math.random() * (max - min)) + min;
+    var colGreen = Math.floor(Math.random() * (max - min)) + min;
+
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
+
+    return color;
+}
+/**
+ * Funktion für die zufällige helle-(Pastel)Farbeerstellung
+ */
+
+function colorCreatePastel() {
+    var color;
+    var max = 256;
+    var min = 180;
+
+    var colRed = Math.floor(Math.random() * (max - min)) + min;
+    var colBlue = Math.floor(Math.random() * (max - min)) + min;
+    var colGreen = Math.floor(Math.random() * (max - min)) + min;
+
+    color = "rgb(" + colRed + "," + colGreen + "," + colBlue + ")";
+
+    return color;
+}
